8ff4f1cf66bef863236b1f6f0a3b70eaaf48fac8
reikemdev <jimmymeneses11@gmail.com>
2025-09-04 16:48:00 -0400

    add  module paage

--- valparaiso-presentation/astro.config.mjs
+++ valparaiso-presentation/astro.config.mjs
@@ -1,11 +1,25 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
 
+import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
+
+
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [
+    icon({
+      include: {
+        // Include only three `mdi` icons in the bundle
+        mdi: ['account', 'account-plus', 'account-minus'],
+        // Include all `uis` icons
+        uis: ['*']
+      }
+    })
+  ]
 });
+
+
